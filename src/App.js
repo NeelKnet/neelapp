0d79ed13f86e756366e202d05e7dd2ec78ed2bcb
NeelKnet <67813316+NeelKnet@users.noreply.github.com>
2021-03-12 16:36:20 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Neelapp
+          CLoudFront
         </p>
         <a
           className="App-link"
